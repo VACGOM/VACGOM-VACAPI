e71e9dc21bbb85e88cc3086a12db4a03c3d35558
HyungJu <sungkisa@naver.com>
2024-05-02 19:25:09 +0900

    feat: 주민번호 모델 추가

---
+++ libs/types/src/models/common/identity.ts
@@ -0,0 +1,45 @@
+import { LocalDate } from 'js-joda';
+import { InvalidRnnSecretException } from '../../exceptions/InvalidRnnSecretException';
+
+export class Identity {
+  private birthDate!: LocalDate; //생년월일
+  private rnnSecret?: string; //주민등록번호 뒷자리
+
+  constructor(birthDate: LocalDate, rnnSecret?: string) {
+    if (rnnSecret && rnnSecret.length != 7)
+      throw new InvalidRnnSecretException();
+
+    this.birthDate = birthDate;
+    this.rnnSecret = rnnSecret;
+  }
+
+  static fromRnnString(rnn: string): Identity {
+    const matches = rnn.match(/(\d{6})-*(\d{7})/g);
+    if (!matches || matches.length != 2) throw new InvalidRnnSecretException();
+
+    const birthDate = LocalDate.parse(matches[0]);
+    const rnnSecret = matches[1];
+
+    return new Identity(birthDate, rnnSecret);
+  }
+
+  static isBornIn1900s(rnnSecret: string): boolean {
+    if (rnnSecret.startsWith('1') || rnnSecret.startsWith('2')) return true;
+    else return false;
+  }
+
+  static fromPartialRnnString(partialRnn: string): Identity {
+    //partialRnn : 030912-3 or 0309123
+
+    const matches = partialRnn.match(/(\d{6})-*(\d)/g);
+    if (!matches || matches.length != 2) throw new InvalidRnnSecretException();
+
+    const bornIn1900s = this.isBornIn1900s(matches[1]);
+
+    const birthDate = LocalDate.parse(
+      `${bornIn1900s ? '19' : '20'}${matches[0]}`
+    );
+
+    return new Identity(birthDate);
+  }
+}
