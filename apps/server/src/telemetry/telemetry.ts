032c9e1817badc4157b11a7646ec25e518da8836
HyungJu <sungkisa@naver.com>
2024-05-23 16:27:21 +0900

    feat: use prometheus exporter

--- apps/server/src/telemetry/telemetry.ts
+++ apps/server/src/telemetry/telemetry.ts
@@ -1,15 +1,11 @@
-import {
-  ConsoleMetricExporter,
-  PeriodicExportingMetricReader,
-} from '@opentelemetry/sdk-metrics';
 import { Resource } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import {
   SEMRESATTRS_SERVICE_NAME,
   SEMRESATTRS_SERVICE_VERSION,
 } from '@opentelemetry/semantic-conventions';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 
 export function initTelemetry() {
   const sdk = new NodeSDK({
@@ -18,9 +14,8 @@ export function initTelemetry() {
       [SEMRESATTRS_SERVICE_VERSION]: '1.0',
     }),
     instrumentations: [getNodeAutoInstrumentations()],
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-      exporter: new ConsoleMetricExporter(),
+    metricReader: new PrometheusExporter({
+      port: 9464,
     }),
   });
 
